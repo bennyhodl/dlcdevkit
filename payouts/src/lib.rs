fd2eb0b452fd2e6d2c29508d91a89849500ad33d
bennyhodl <ben@bitcoinbay.live>
2023-12-13 22:33:13 -0500

    Payouts init

---
+++ payouts/src/lib.rs
@@ -0,0 +1,7 @@
+use dlc_manager::contract::{contract_input::ContractInput, enum_descriptor::EnumDescriptor};
+use dlc_messages::oracle_msgs::OracleAnnouncement;
+
+
+pub fn create_enum_contract_inputs(_collateral: u64, _fee_rate: u64, _descriptor: EnumDescriptor, _oracle_announcement: OracleAnnouncement) {
+    ()
+}
