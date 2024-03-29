efb26fe4f8f13963f76fade1e92e6dc2ce58573a
bennyhodl <ben@bitcoinbay.live>
2024-03-29 11:51:27 -0400

    Sync function for ernest calls

--- terminal/src-tauri/src/dlc.rs
+++ terminal/src-tauri/src/dlc.rs
@@ -15,22 +15,21 @@ pub fn list_contracts(ernest: State<Arc<Ernest>>, peer_manager: State<Arc<Ernest
         &peer_manager.message_handler,
     );
     let ernest_clone = ernest.manager.clone();
-    tokio::task::spawn_blocking(move || {
-        ernest_clone.lock().unwrap().periodic_check(false).unwrap();
-        let contracts = ernest_clone
-            .lock()
-            .unwrap()
-            .get_store()
-            .get_contracts()
-            .unwrap();
-        println!("CONTRACTS: {:?}", contracts);
-    });
+    ernest_clone.lock().unwrap().periodic_check(false).unwrap();
+    let contracts = ernest_clone
+        .lock()
+        .unwrap()
+        .get_store()
+        .get_contracts()
+        .unwrap();
+    println!("{:?}", contracts);
+    ()
 }
 
 #[tauri::command]
-pub async fn list_offers(
-    ernest: State<'_, Arc<Ernest>>,
-    peer_manager: State<'_, Arc<ErnestPeerManager>>,
+pub fn list_offers(
+    ernest: State<Arc<Ernest>>,
+    peer_manager: State<Arc<ErnestPeerManager>>,
 ) -> Result<Vec<OfferedContract>, tauri::Error> {
     process_incoming_messages(
         &peer_manager.peer_manager,
@@ -38,18 +37,41 @@ pub async fn list_offers(
         &peer_manager.message_handler,
     );
     let ernest_clone = ernest.manager.clone();
-    let offers = tokio::task::spawn_blocking(move || {
-        ernest_clone.lock().unwrap().periodic_check(false).unwrap();
-        let offers = ernest_clone
-            .lock()
-            .unwrap()
-            .get_store()
-            .get_contract_offers()
-            .unwrap();
-        offers
-    })
-    .await
-    .unwrap();
+    ernest_clone.lock().unwrap().periodic_check(false).unwrap();
+    let offers = ernest_clone
+        .lock()
+        .unwrap()
+        .get_store()
+        .get_contract_offers()
+        .unwrap();
 
     Ok(offers)
 }
+
+// ASYNC example
+// #[tauri::command]
+// pub async fn list_offers(
+//     ernest: State<'_, Arc<Ernest>>,
+//     peer_manager: State<'_, Arc<ErnestPeerManager>>,
+// ) -> Result<Vec<OfferedContract>, tauri::Error> {
+//     process_incoming_messages(
+//         &peer_manager.peer_manager,
+//         &ernest.manager,
+//         &peer_manager.message_handler,
+//     );
+//     let ernest_clone = ernest.manager.clone();
+//     let offers = tokio::task::spawn_blocking(move || {
+//         ernest_clone.lock().unwrap().periodic_check(false).unwrap();
+//         let offers = ernest_clone
+//             .lock()
+//             .unwrap()
+//             .get_store()
+//             .get_contract_offers()
+//             .unwrap();
+//         offers
+//     })
+//     .await
+//     .unwrap();
+//
+//     Ok(offers)
+// }
