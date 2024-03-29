d2ae2ae4bf48573e0c4164ab290ce69fdef77e71
bennyhodl <ben@bitcoinbay.live>
2024-03-29 11:51:42 -0400

    Moved DLC wallet traits to wallet

--- wallet/src/wallet.rs
+++ wallet/src/wallet.rs
@@ -1,4 +1,4 @@
-use crate::io;
+use crate::{error::bdk_err_to_manager_err, io};
 use anyhow::anyhow;
 use bdk::{
     bitcoin::{
@@ -12,10 +12,12 @@ use bdk::{
     wallet::{AddressIndex, AddressInfo},
     Balance, FeeRate, KeychainKind, SignOptions, SyncOptions, Wallet,
 };
-use lightning::chain::chaininterface::ConfirmationTarget;
+use bitcoin::ScriptBuf;
+use dlc_manager::{error::Error as ManagerError, SimpleSigner};
+use lightning::chain::chaininterface::{ConfirmationTarget, FeeEstimator};
 use serde::Deserialize;
 use sled::Tree;
-use std::sync::Arc;
+use std::sync::{atomic::Ordering, Arc};
 use std::sync::{atomic::AtomicU32, RwLock};
 use std::{collections::HashMap, sync::Mutex};
 
@@ -27,6 +29,7 @@ pub struct ErnestWallet {
     pub network: Network,
     pub xprv: ExtendedPrivKey,
     pub name: String,
+    pub fees: Arc<HashMap<ConfirmationTarget, AtomicU32>>,
     secp: Secp256k1<All>,
 }
 
@@ -76,13 +79,14 @@ impl ErnestWallet {
             ConfirmationTarget::ChannelCloseMinimum,
             AtomicU32::new(MIN_FEERATE),
         );
-        let _fees = Arc::new(fees);
+        let fees = Arc::new(fees);
 
         Ok(ErnestWallet {
             blockchain,
             inner,
             network,
             xprv,
+            fees,
             secp,
             name: name.to_string(),
         })
@@ -150,5 +154,107 @@ impl ErnestWallet {
         };
 
         Ok(tx.txid())
+    }
+}
+
+impl FeeEstimator for ErnestWallet {
+    fn get_est_sat_per_1000_weight(&self, confirmation_target: ConfirmationTarget) -> u32 {
+        self.fees
+            .get(&confirmation_target)
+            .unwrap()
+            .load(Ordering::Acquire)
+    }
+}
+
+impl dlc_manager::ContractSignerProvider for ErnestWallet {
+    type Signer = SimpleSigner;
+
+    fn derive_signer_key_id(&self, _is_offer_party: bool, temp_id: [u8; 32]) -> [u8; 32] {
+        temp_id
+    }
+
+    fn derive_contract_signer(&self, _key_id: [u8; 32]) -> Result<Self::Signer, ManagerError> {
+        Ok(SimpleSigner::new(self.xprv.private_key))
+    }
+
+    fn get_secret_key_for_pubkey(
+        &self,
+        _pubkey: &bitcoin::secp256k1::PublicKey,
+    ) -> Result<bitcoin::secp256k1::SecretKey, ManagerError> {
+        unimplemented!()
+    }
+
+    fn get_new_secret_key(&self) -> Result<bitcoin::secp256k1::SecretKey, ManagerError> {
+        unimplemented!()
+    }
+}
+
+impl dlc_manager::Wallet for ErnestWallet {
+    fn get_new_address(&self) -> Result<bitcoin::Address, ManagerError> {
+        Ok(self
+            .new_external_address()
+            .map_err(bdk_err_to_manager_err)?
+            .address)
+    }
+
+    fn get_new_change_address(&self) -> Result<bitcoin::Address, ManagerError> {
+        Ok(self
+            .new_change_address()
+            .map_err(bdk_err_to_manager_err)?
+            .address)
+    }
+
+    // TODO: Is this correct for the input?
+    fn sign_psbt_input(
+        &self,
+        psbt: &mut bitcoin::psbt::PartiallySignedTransaction,
+        _input_index: usize,
+    ) -> Result<(), ManagerError> {
+        self.inner
+            .lock()
+            .unwrap()
+            .sign(psbt, bdk::SignOptions::default())
+            .map_err(bdk_err_to_manager_err)?;
+        Ok(())
+    }
+
+    // TODO: Does BDK have reserved UTXOs?
+    fn unreserve_utxos(&self, _outpoints: &[bitcoin::OutPoint]) -> Result<(), ManagerError> {
+        Ok(())
+    }
+
+    fn import_address(&self, _address: &bitcoin::Address) -> Result<(), ManagerError> {
+        // might be ok, might not
+        Ok(())
+    }
+
+    // return all utxos
+    // fixme use coin selector
+    fn get_utxos_for_amount(
+        &self,
+        _amount: u64,
+        _fee_rate: u64,
+        _lock_utxos: bool,
+    ) -> Result<Vec<dlc_manager::Utxo>, ManagerError> {
+        let wallet = self.inner.lock().unwrap();
+
+        let local_utxos = wallet.list_unspent().map_err(bdk_err_to_manager_err)?;
+
+        let dlc_utxos = local_utxos
+            .iter()
+            .map(|utxo| {
+                let address =
+                    Address::from_script(&utxo.txout.script_pubkey, self.network).unwrap();
+                dlc_manager::Utxo {
+                    tx_out: utxo.txout.clone(),
+                    outpoint: utxo.outpoint,
+                    address,
+                    redeem_script: ScriptBuf::new(),
+                    reserved: false,
+                }
+            })
+            .collect();
+
+        Ok(dlc_utxos)
     }
 }
