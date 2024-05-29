881d9ce6e927c64a7ef39516beff851518cb6511
benny b <ben@bitcoinbay.foundation>
2024-05-29 15:43:10 -0400

    More verbose example API. not there yet tho.

---
+++ ddk/examples/ddk.rs
@@ -0,0 +1,37 @@
+use dlc_dev_kit::builder::DdkBuilder;
+use dlc_dev_kit::{DdkOracle, DdkStorage, DdkTransport};
+use std::sync::Arc;
+
+#[derive(Clone)]
+struct MockTransport;
+impl DdkTransport for MockTransport {}
+
+#[derive(Clone)]
+struct MockStorage;
+impl DdkStorage for MockStorage {}
+
+#[derive(Clone)]
+struct MockOracle;
+impl DdkOracle for MockOracle {}
+
+type ApplicationDdk = dlc_dev_kit::DlcDevKit<MockTransport, MockStorage, MockOracle>;
+
+#[tokio::main]
+async fn main() {
+    let transport = Arc::new(MockTransport {});
+    let storage = Arc::new(MockStorage {});
+    let oracle_client = Arc::new(MockOracle {});
+    let builder: ApplicationDdk = DdkBuilder::new()
+        .set_name("ddk")
+        .set_esplora_url("https://mempool.space/api")
+        .set_transport(transport.clone())
+        .set_storage(storage.clone())
+        .set_oracle(oracle_client.clone())
+        .finish()
+        .await
+        .unwrap();
+
+    let wallet = builder.wallet.new_external_address();
+
+    assert!(wallet.is_ok());
+}
