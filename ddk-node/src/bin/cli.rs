82fa5b0308e366454cabeeb488247989b03704b8
bennyhodl <ben@bitcoinbay.foundation>
2024-07-26 12:10:50 +0200

    cli init

--- ddk-node/src/bin/cli.rs
+++ ddk-node/src/bin/cli.rs
@@ -1,3 +1,34 @@
-fn main() {
+use clap::{Parser, Subcommand};
+use ddk_node::ddkrpc::ddk_rpc_client::DdkRpcClient;
+use ddk_node::ddkrpc::InfoRequest;
+
+#[derive(Debug, Clone, Parser)]
+#[clap(author, version, about)]
+struct  DdkCliArgs {
+    #[clap(subcommand)]
+    pub command: CliCommand
+}
+
+#[derive(Debug, Clone, Subcommand)]
+enum CliCommand {
+    // Gets information about the DDK instance
+    Info
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let args = DdkCliArgs::parse();
+
+    let mut client = DdkRpcClient::connect("http://127.0.0.1:3030").await?;
+
+    match args.command {
+        CliCommand::Info => {
+            let info = client.info(InfoRequest::default()).await?.into_inner();
+            println!("{:?}", info);
+        }
+    }
+
     println!("heyhowareya cli");
+
+    Ok(())
 }
