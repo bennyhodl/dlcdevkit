d2da08026ba37e7b9f96252c6e0a0cee4070b603
bennyhodl <ben@bitcoinbay.foundation>
2024-12-17 19:43:24 -0500

    Lightning Transport v2

--- ddk/src/transport/lightning/peer_manager.rs
+++ ddk/src/transport/lightning/peer_manager.rs
@@ -9,8 +9,14 @@ use lightning::{
     sign::{KeysManager, NodeSigner},
     util::logger::{Logger, Record},
 };
-use lightning_net_tokio::SocketDescriptor;
-use std::{sync::Arc, time::SystemTime};
+use lightning_net_tokio::{setup_inbound, SocketDescriptor};
+use std::{
+    sync::Arc,
+    time::{Duration, SystemTime},
+};
+use tokio::{net::TcpListener, sync::watch, task::JoinHandle, time::interval};
+
+use crate::{ddk::DlcDevKitDlcManager, Oracle, Storage};
 
 pub struct DlcDevKitLogger;
 
@@ -76,5 +82,223 @@ impl LightningTransport {
             node_id,
             listening_port,
         })
+    }
+
+    pub fn listen(
+        &self,
+        stop_signal: watch::Receiver<bool>,
+    ) -> JoinHandle<Result<(), anyhow::Error>> {
+        let listening_port = self.listening_port;
+        let mut listen_stop = stop_signal.clone();
+        let peer_manager = Arc::clone(&self.peer_manager);
+        tokio::spawn(async move {
+            let listener = TcpListener::bind(format!("0.0.0.0:{}", listening_port))
+                .await
+                .expect("Coldn't get port.");
+
+            tracing::info!(
+                addr =? listener.local_addr().unwrap(),
+                "Starting lightning peer manager listener."
+            );
+            loop {
+                tokio::select! {
+                    _ = listen_stop.changed() => {
+                        if *listen_stop.borrow() {
+                            tracing::warn!("Stop signal for lightning connection manager.");
+                            break;
+                        }
+                    },
+                    accept_result = listener.accept() => {
+                        match accept_result {
+                            Ok((tcp_stream, socket)) => {
+                                let peer_mgr = Arc::clone(&peer_manager);
+                                tokio::spawn(async move {
+                                    tracing::info!(
+                                        connection = socket.to_string(),
+                                        "Received connection."
+                                    );
+                                    setup_inbound(peer_mgr, tcp_stream.into_std().unwrap()).await;
+                                });
+                            }
+                            Err(e) => {
+                                tracing::error!("Error accepting connection: {}", e);
+                            }
+                        }
+                    }
+                }
+            }
+            Ok::<_, anyhow::Error>(())
+        })
+    }
+
+    pub fn process_messages<S: Storage, O: Oracle>(
+        &self,
+        stop_signal: watch::Receiver<bool>,
+        manager: Arc<DlcDevKitDlcManager<S, O>>,
+        peer_manager: Arc<LnPeerManager>,
+    ) -> JoinHandle<Result<(), anyhow::Error>> {
+        let mut message_stop = stop_signal.clone();
+        let message_manager = Arc::clone(&manager);
+        // let peer_manager = Arc::clone(&self.peer_manager);
+        let message_handler = Arc::clone(&self.message_handler);
+        tokio::spawn(async move {
+            let mut message_interval = interval(Duration::from_secs(5));
+            let mut event_interval = interval(Duration::from_secs(2));
+            loop {
+                tokio::select! {
+                    _ = message_stop.changed() => {
+                        if *message_stop.borrow() {
+                            tracing::warn!("Stop signal for lightning message processor.");
+                            break;
+                        }
+                    },
+                    _ = message_interval.tick() => {
+                        let messages = message_handler.get_and_clear_received_messages();
+                        for (counter_party, message) in messages {
+                            tracing::info!(
+                                counter_party = counter_party.to_string(),
+                                "Processing DLC message"
+                            );
+                            match message_manager.on_dlc_message(&message, counter_party).await {
+                                Ok(Some(response)) => {
+                                    message_handler.send_message(counter_party, response);
+                                }
+                                Ok(None) => (),
+                                Err(e) => {
+                                    tracing::error!(
+                                        error=e.to_string(),
+                                        counterparty=counter_party.to_string(),
+                                        message=?message,
+                                        "Could not process dlc message."
+                                    );
+                                }
+                            }
+                        }
+                    }
+                    _ = event_interval.tick() => {
+                        peer_manager.process_events()
+                    }
+                }
+            }
+            Ok::<_, anyhow::Error>(())
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use bitcoin::Network;
+    use ddk_manager::Storage;
+    use dlc_messages::{Message, OfferDlc};
+
+    use crate::{
+        builder::Builder, oracle::memory::MemoryOracle, storage::memory::MemoryStorage, DlcDevKit,
+        Transport,
+    };
+
+    use super::*;
+
+    fn get_offer() -> OfferDlc {
+        let offer_string = include_str!("../../../../ddk-manager/test_inputs/offer_contract.json");
+        let offer: OfferDlc = serde_json::from_str(&offer_string).unwrap();
+        offer
+    }
+
+    fn create_peer_manager(listening_port: u16) -> (LightningTransport, PublicKey) {
+        let mut seed = [0u8; 32];
+        seed.try_fill(&mut bitcoin::key::rand::thread_rng())
+            .unwrap();
+        let peer_manager = LightningTransport::new(&seed, listening_port).unwrap();
+        let pubkey = peer_manager.node_id.clone();
+        (peer_manager, pubkey)
+    }
+
+    async fn manager(
+        listening_port: u16,
+    ) -> DlcDevKit<LightningTransport, MemoryStorage, MemoryOracle> {
+        let mut seed_bytes = [0u8; 32];
+        seed_bytes
+            .try_fill(&mut bitcoin::key::rand::thread_rng())
+            .unwrap();
+
+        let transport = Arc::new(LightningTransport::new(&seed_bytes, listening_port).unwrap());
+        let storage = Arc::new(MemoryStorage::new());
+        let oracle_client = Arc::new(MemoryOracle::default());
+
+        let mut builder = Builder::new();
+        builder.set_network(Network::Regtest);
+        builder.set_esplora_host("http://127.0.0.1:30000".to_string());
+        builder.set_seed_bytes(seed_bytes);
+        builder.set_transport(transport.clone());
+        builder.set_storage(storage.clone());
+        builder.set_oracle(oracle_client.clone());
+        builder.finish().await.unwrap()
+    }
+
+    #[test_log::test(tokio::test)]
+    async fn send_offer() {
+        let alice = manager(1776).await;
+        let alice_pk = alice.transport.public_key();
+        let bob = manager(1777).await;
+        let _bob_pk = bob.transport.public_key();
+
+        bob.start().unwrap();
+        alice.start().unwrap();
+
+        bob.transport
+            .connect_outbound(alice_pk, "127.0.0.1:1776")
+            .await;
+
+        let mut connected = false;
+        let mut retries = 0;
+
+        while !connected {
+            if retries > 10 {
+                bob.stop().unwrap();
+                alice.stop().unwrap();
+                panic!("Bob could not connect to alice.")
+            }
+            if bob
+                .transport
+                .peer_manager
+                .peer_by_node_id(&alice_pk)
+                .is_some()
+            {
+                connected = true
+            }
+            retries += 1;
+            tokio::time::sleep(Duration::from_millis(100)).await
+        }
+
+        let offer = get_offer();
+        bob.transport
+            .send_message(alice_pk, Message::Offer(offer.clone()));
+
+        let mut offer_received = false;
+        let mut retries = 0;
+
+        while !offer_received {
+            if retries > 15 {
+                bob.stop().unwrap();
+                alice.stop().unwrap();
+                panic!("Contract was not offered to alice")
+            }
+            if alice
+                .storage
+                .get_contract_offers()
+                .unwrap()
+                .iter()
+                .find(|o| o.id == offer.temporary_contract_id)
+                .is_some()
+            {
+                offer_received = true
+            }
+            retries += 1;
+            tokio::time::sleep(Duration::from_secs(1)).await
+        }
+
+        bob.stop().unwrap();
+        alice.stop().unwrap();
+        assert!(true)
     }
 }
---
+++ ddk/tests/nostr.rs
@@ -0,0 +1,107 @@
+mod test_util;
+
+#[cfg(feature = "nostr")]
+mod nostr_test {
+    use super::*;
+    use bitcoin::{key::rand::Fill, secp256k1::PublicKey, Network};
+    use chrono::{Local, TimeDelta};
+    use ddk::builder::Builder;
+    use ddk::oracle::memory::MemoryOracle;
+    use ddk::storage::memory::MemoryStorage;
+    use ddk::transport::nostr::NostrDlc;
+    use ddk::DlcDevKit;
+    use dlc::{EnumerationPayout, Payout};
+    use std::sync::Arc;
+
+    type NostrDlcDevKit = DlcDevKit<NostrDlc, MemoryStorage, MemoryOracle>;
+
+    async fn nostr_ddk(name: &str, oracle: Arc<MemoryOracle>) -> NostrDlcDevKit {
+        let mut seed = [0u8; 32];
+        seed.try_fill(&mut bitcoin::key::rand::thread_rng())
+            .unwrap();
+        let esplora_host = "http://127.0.0.1:30000".to_string();
+
+        let transport = Arc::new(
+            NostrDlc::new(&seed, "wss://nostr.dlcdevkit.com", Network::Regtest)
+                .await
+                .unwrap(),
+        );
+        let storage = Arc::new(MemoryStorage::new());
+
+        let ddk: NostrDlcDevKit = Builder::new()
+            .set_network(Network::Regtest)
+            .set_seed_bytes(seed)
+            .set_esplora_host(esplora_host)
+            .set_name(name)
+            .set_oracle(oracle)
+            .set_transport(transport)
+            .set_storage(storage)
+            .finish()
+            .await
+            .unwrap();
+        ddk
+    }
+
+    const EVENT_ID: &str = "nostr-event";
+
+    #[test_log::test(tokio::test)]
+    async fn nostr_contract() {
+        let oracle = Arc::new(MemoryOracle::default());
+        let alice = nostr_ddk("alice-nostr", oracle.clone()).await;
+        let bob = nostr_ddk("bob-nostr", oracle.clone()).await;
+
+        alice.start().unwrap();
+        bob.start().unwrap();
+
+        let alice_address = alice.wallet.new_external_address().unwrap().address;
+        let bob_address = bob.wallet.new_external_address().unwrap().address;
+        test_util::fund_addresses(&alice_address, &bob_address);
+
+        let expiry = TimeDelta::seconds(15);
+        let timestamp: u32 = Local::now()
+            .checked_add_signed(expiry)
+            .unwrap()
+            .timestamp()
+            .try_into()
+            .unwrap();
+
+        let announcement = oracle
+            .oracle
+            .create_enum_event(
+                "nostr-event".to_string(),
+                vec!["cat".to_string(), "ctv".to_string()],
+                timestamp,
+            )
+            .await
+            .unwrap();
+
+        let contract_input = ddk_payouts::enumeration::create_contract_input(
+            vec![
+                EnumerationPayout {
+                    outcome: "cat".to_string(),
+                    payout: Payout {
+                        offer: 100_000_000,
+                        accept: 0,
+                    },
+                },
+                EnumerationPayout {
+                    outcome: "ctv".to_string(),
+                    payout: Payout {
+                        offer: 0,
+                        accept: 100_000_000,
+                    },
+                },
+            ],
+            100_000_000,
+            100_000_000,
+            1,
+            oracle.oracle.public_key().to_string(),
+            EVENT_ID.to_string(),
+        );
+        let alice_pubkey =
+            PublicKey::from_slice(&alice.transport.public_key().serialize()).expect("cant convert");
+        let _offer = bob
+            .send_dlc_offer(&contract_input, alice_pubkey, vec![announcement])
+            .unwrap();
+    }
+}
